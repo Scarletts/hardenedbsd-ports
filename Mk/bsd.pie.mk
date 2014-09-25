49b1b63734b3ee3890ed2351c2637b77049108c0
Shawn Webb <lattera@gmail.com>
2014-09-25 08:47:11 -0400

    Remove unneeded comment

--- Mk/bsd.pie.mk
+++ Mk/bsd.pie.mk
@@ -5,7 +5,6 @@ PIE_Include_MAINTAINER=	shawn.webb@hardenedbsd.org
 
 .if !defined(WITHOUT_PIE) && \
     (${ARCH} == i386 || ${ARCH} == amd64)
-# Overridable as a user may want to use -fstack-protector-all
 PIE_CFLAGS?=	-fPIE
 CFLAGS+=	${PIE_CFLAGS}
 CXXFLAGS+=	${PIE_CFLAGS}
